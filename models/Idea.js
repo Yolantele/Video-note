1e1d696042286808224991cc6b348cab3e9f683d
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-27 19:12:32 +0000

    new Schema for Idea model

---
+++ models/Idea.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Create Schema
+var IdeaSchema = new Schema({
+  title: {
+    type: String,
+    required:true
+  },
+  details:{
+    type:String,
+    required: true
+  },
+  date:{
+    type: Date,
+    default: Date.now
+  }
+});
+mongoose.model('ideas', IdeaSchema);
