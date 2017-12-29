85de2d09c714d32090350e9c64f70249e7af8da3
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-29 18:32:13 +0000

    Save for deploying on heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
   module.exports = {
-    mongoURI: 'mongodb://jolanta:jolanta@ds231987.mlab.com:31987/jolantajas'}
+    mongoURI: 'mongodb://jolanta:jolanta@ds231987.mlab.com:31987/jolantajas'};
 } else {
   module.exports = {
-    mongoURI: 'mongodb://jolanta:jolanta@ds231987.mlab.com:31987/jolantajas'}
+    mongoURI: 'mongodb://jolanta:jolanta@ds231987.mlab.com:31987/jolantajas'};
 }
